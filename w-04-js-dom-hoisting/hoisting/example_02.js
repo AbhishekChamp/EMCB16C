75395cccf8d3d65abce3629e8d61961a6dc1ef01
AbhishekChamp <abhishek96.r@gmail.com>
2025-02-09 20:51:10 +0530

    Week-04 Live class

---
+++ w-04-js-dom-hoisting/hoisting/example_02.js
@@ -0,0 +1,7 @@
+console.log(myLet);
+let myLet = 5;
+console.log(myLet);
+
+console.log(myConst);
+const myConst = 20;
+console.log(myConst);
---
+++ w-04-js-dom-hoisting/hoisting/example_04.js
@@ -0,0 +1,8 @@
+// Function Expression Example
+
+console.log(typeof myFunc);
+myFunc();
+
+var myFunc = function () {
+    console.log("Hello, world!");
+};
